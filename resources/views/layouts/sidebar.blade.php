11aced83ddd86d4be87043481fa6434ae737eef5
asrani <asrani.27@gmail.com>
2024-08-14 21:55:51 +0800

    F

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,32 +4,32 @@
 
     @if (Auth::user()->roles == 'superadmin')
     <!-- Superadmin -->
-    <li class="{{ request()->is('superadmin/beranda') ? 'active' : '' }}"><a href="/superadmin/beranda"><i class="fa fa-dashboard"></i> <span>Beranda</span></a></li>
+    <li class="{{ request()->is('superadmin/beranda') ? 'active' : '' }}"><a href="/superadmin/beranda"><i class="fa fa-dashboard"></i> <span>BERANDA</span></a></li>
     <li class="{{ request()->is('superadmin/portal/*') ? 'active' : '' }} treeview">
         <a href="#">
-        <i class="fa fa-database"></i> <span>Portal</span>
+        <i class="fa fa-database"></i> <span>PORTAL</span>
         <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
         </span>
         </a>
         <ul class="treeview-menu">
-        <li class="{{ request()->is('superadmin/portal/informasi*') ? 'active' : '' }}"><a href="/superadmin/portal/informasi"><i class="fa fa-circle {{ request()->is('superadmin/portal/informasi*') ? 'text-blue' : '' }}"></i> Informasi</a></li>
+        <li class="{{ request()->is('superadmin/portal/informasi*') ? 'active' : '' }}"><a href="/superadmin/portal/informasi"><i class="fa fa-circle {{ request()->is('superadmin/portal/informasi*') ? 'text-blue' : '' }}"></i> INFORMASI</a></li>
 
         </ul>
     </li>
     <li class="{{ request()->is('superadmin/data/*') ? 'active' : '' }} treeview">
         <a href="#">
-        <i class="fa fa-database"></i> <span>Data</span>
+        <i class="fa fa-database"></i> <span>DATA</span>
         <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
         </span>
         </a>
         <ul class="treeview-menu">
-        <li class="{{ request()->is('superadmin/data/periode*') ? 'active' : '' }}"><a href="/superadmin/data/periode"><i class="fa fa-circle {{ request()->is('superadmin/data/periode*') ? 'text-blue' : '' }}"></i> Periode</a></li>
-        <li class="{{ request()->is('superadmin/data/jurusan*') ? 'active' : '' }}"><a href="/superadmin/data/jurusan"><i class="fa fa-circle {{ request()->is('superadmin/data/jurusan*') ? 'text-blue' : '' }}"></i> Jurusan</a></li>
-        <li class="{{ request()->is('superadmin/data/matakuliah') ? 'active' : '' }}"><a href="/superadmin/data/matakuliah"><i class="fa fa-circle {{ request()->is('superadmin/data/matakuliah') ? 'text-blue' : '' }}"></i> Mata Kuliah</a></li>
-        <li class="{{ request()->is('superadmin/data/kurikulum') ? 'active' : '' }}"><a href="/superadmin/data/kurikulum"><i class="fa fa-circle {{ request()->is('superadmin/data/kurikulum') ? 'text-blue' : '' }}"></i> Kurikulum</a></li>
-        <li class="{{ request()->is('superadmin/data/mahasiswa') ? 'active' : '' }}"><a href="/superadmin/data/mahasiswa"><i class="fa fa-circle {{ request()->is('superadmin/data/mahasiswa') ? 'text-blue' : '' }}"></i> Mahasiswa</a></li>
+        <li class="{{ request()->is('superadmin/data/periode*') ? 'active' : '' }}"><a href="/superadmin/data/periode"><i class="fa fa-circle {{ request()->is('superadmin/data/periode*') ? 'text-blue' : '' }}"></i> PERIODE</a></li>
+        <li class="{{ request()->is('superadmin/data/jurusan*') ? 'active' : '' }}"><a href="/superadmin/data/jurusan"><i class="fa fa-circle {{ request()->is('superadmin/data/jurusan*') ? 'text-blue' : '' }}"></i> JURUSAN</a></li>
+        <li class="{{ request()->is('superadmin/data/matakuliah') ? 'active' : '' }}"><a href="/superadmin/data/matakuliah"><i class="fa fa-circle {{ request()->is('superadmin/data/matakuliah') ? 'text-blue' : '' }}"></i> MATA KULIAH</a></li>
+        <li class="{{ request()->is('superadmin/data/kurikulum') ? 'active' : '' }}"><a href="/superadmin/data/kurikulum"><i class="fa fa-circle {{ request()->is('superadmin/data/kurikulum') ? 'text-blue' : '' }}"></i> KURIKULUM</a></li>
+        <li class="{{ request()->is('superadmin/data/mahasiswa') ? 'active' : '' }}"><a href="/superadmin/data/mahasiswa"><i class="fa fa-circle {{ request()->is('superadmin/data/mahasiswa') ? 'text-blue' : '' }}"></i> MAHASISWA</a></li>
 
         </ul>
     </li>
